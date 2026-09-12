9eb5487eaa13c7e6aa49d7b7d39f20fead720234
G.H.O.S.T0_o <69056141+1cey428@users.noreply.github.com>
2026-09-11 22:51:47 -0400

    Enhance Base64 encoder with Base16 and Base32 support
    Refactored encoding and decoding functions to support Base16, Base32, and Base64. Updated main function to allow user to select encoding/decoding base.

---
+++ Cyber Security Tools/Encoding/Base64/Encoder.py
@@ -0,0 +1,70 @@
+import base64
+#base 64 fucntions
+def b64_encoder():
+    text=input("What would you like to convert: ")
+    text = text.encode("ASCII")
+    conv_text = base64.b64encode(text).decode()
+    text = text.decode()
+    print("")
+    print(str(text)+ " converted is " + str(conv_text))
+
+
+def b64_decoder():
+    encoded_text=input("What would you like me to decode: ")
+    decoded_text = base64.b64decode(encoded_text)
+    print(str(encoded_text) + " Decoded is " + str(decoded_text))
+
+#base 32 fuctions
+def b32_encoder():
+    text32=input("What would you like to convert: ")
+    text32=text32.encode("ASCII")
+    conv_text32 = base64.b32encode(text32).decode()
+    text32 = text32.decode()
+    print("")
+    print(str(text32)+ " converted is " + str(conv_text32))
+
+def b32_decoder():
+    encoded_text32=input("What would you like me to decode: ")
+    decoded_text32= base64.b32decode(encoded_text32)
+    print(str(encoded_text32) + " Decoded is " + str(decoded_text32))
+
+#base 16 fuctions 
+def b16_encoder():
+    text16=input("What would you like to convert: ")
+    text16 = text16.encode("ASCII")
+    conv_text16 = base64.b16encode(text16).decode()
+    text16 = text16.decode()
+    print("")
+    print(str(text16)+ " converted is " + str(conv_text16))
+
+def b16_decoder():
+    encoded_text16=input("What would you like me to decode: ")
+    decoded_text16= base64.b16decode(encoded_text16)
+    print(str(encoded_text16) + " Decoded is " + str(decoded_text16))
+
+#Main fuction
+def Encoder():
+    print("Welcome to my Base64 converter! \n 1.Encode text \n 2.Encode image(WIP) \n 3.Decode Text \n 4.Decode Image" ) 
+    user_input = int(input("what can I do for you: "))
+    if user_input == 1:
+        print("")
+        base_selection = int(input("What base do you want to use \n 1.Base 16 \n 2.Base 32 \n 3.Base 64 \n Selection(Pick a Number):"))
+        if base_selection == 1:
+            b16_encoder()
+        elif base_selection == 2:
+            b32_encoder()
+        elif base_selection == 3:
+            b64_encoder()
+    elif user_input ==2:
+        print("WIP")
+    elif user_input ==3:
+        print("")
+        base_selection2 = int(input("What is it in \n 1.Base 16 \n 2.Base 32 \n 3.Base 64 \n Selection(Pick a Number):"))
+        if base_selection2 == 1:
+            b16_decoder()
+        elif base_selection2 == 2:
+            b32_decoder()
+        elif base_selection2 ==3:
+            b64_decoder()
+        
+Encoder()
